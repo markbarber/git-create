5ecf91155404c166454dc692552aeb88e69b794b
Mark Barber <mbarber@familysearch.org>
2019-12-11 14:57:38 -0700

    DO NOT MERGE THIS
    This is an example of what I did as a test.
    
     ~/forks/git-create ↠  ./.my_commands.sh
    URL: git@github.com:markbarber/temp.git
    Cloning into '/Users/markbarber21/Desktop/temp'...
    remote: Enumerating objects: 3, done.
    remote: Counting objects: 100% (3/3), done.
    remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
    Receiving objects: 100% (3/3), done.

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 function git-create() {
-    source /path/to/.env
+    source .env
 
     path=$1
     repo_name=`echo $path |sed 's/.*\///'`
 
-    url=$(python3 /path/to/create.py $path $repo_name 2>&1)
+    url=$(python create.py $path $repo_name 2>&1)
     echo "URL: ${url}"
 
     git clone $url $path
 }
+
+
+git-create ~/Desktop/temp
